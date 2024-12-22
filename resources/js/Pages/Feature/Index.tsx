a8015bd0a3af6fde3e9d70b53c5af86d1a105670
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-12-22 11:26:59 +0200

    Created FeatureItem

--- resources/js/Pages/Feature/Index.tsx
+++ resources/js/Pages/Feature/Index.tsx
@@ -1,6 +1,8 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import {Head, Link} from '@inertiajs/react';
 import {Feature, PageProps, PaginatedData} from "@/types";
+import {can} from "@/helpers";
+import FeatureItem from "@/Components/FeatureItem";
 
 export default function Index({auth, features}: PageProps<{features: PaginatedData <Feature>}>) {
   return (
@@ -17,7 +19,15 @@ export default function Index({auth, features}: PageProps<{features: PaginatedDa
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
             <div className="p-6 text-gray-900 dark:text-gray-100">
-
+              {can(auth.user, 'manage_features') &&
+                <div className="mb-8">
+                  <Link href={route('feature.create')} className="inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 dark:bg-gray-200 dark:text-gray-800 dark:hover:bg-white dark:focus:bg-white dark:focus:ring-offset-gray-800 dark:active:bg-gray-300">
+                    Create New Feature
+                  </Link>
+                </div>}
+              {features.data.map(feature => (
+                <FeatureItem feature={feature} key={feature.id}/>
+              ))}
             </div>
           </div>
         </div>
