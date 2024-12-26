2d7875f0505ce0246568988f9002b203f2f8d5ca
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-12-26 10:50:27 +0200

    Implementing User role change

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\FeatureController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UpvoteController;
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -16,6 +17,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::middleware(['verified', 'role:' . RolesEnum::Admin->value])->group(function () {
+        Route::get('/user', [UserController::class, 'index'])
+            ->name('user.index');
+        Route::get('/user/{user}/edit', [UserController::class, 'edit'])
+            ->name('user.edit');
+        Route::put('/user/{user}', [UserController::class, 'update'])
+            ->name('user.update');
+    });
 
     Route::middleware(['verified','role:' . RolesEnum::Admin->value])->group(function () {
         Route::get('/dashboard', function () {
